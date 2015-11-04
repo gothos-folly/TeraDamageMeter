31922d37d8b3d341f578e04400b56e579396790f
gothos <gothos.the.jaghut@gmail.com>
2015-11-04 22:26:52 +0100

    Run as admin (required for some features, like pasting)

--- release.bat
+++ release.bat
@@ -4,7 +4,6 @@ set source=.
 set variant=Debug
 rmdir /Q /S "%output%"
 md "%output%
-copy "%source%\Tera.Sniffing\bin\%variant%\*" "%output%\"
 copy "%source%\Tera.DamageMeter\bin\%variant%\*" "%output%\"
 copy "%source%\ReadmeUser.txt" "%output%\readme.txt"
 xcopy /E "%source%\resources" "%output%\resources\"
